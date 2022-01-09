7c0b608ca37344943295e6a84ed20c26a2b518fa
Sélène Mouriaux <magicneuron@gmail.com>
2022-01-09 23:35:48 +0100

    Update exo3.php

--- module1/battle-dev/juin21-ex3/exo3.php
+++ module1/battle-dev/juin21-ex3/exo3.php
@@ -39,29 +39,31 @@ function processAndCheckIO($IOcouples)
 //        var_dump($newGrid);
 //        echo '</pre><hr>';
         for ($x = 0; $x < 10; $x++) {
-            $y = 1;
-            while ($y < 21 && $newGrid['row_' . $y][$x] == '.' ) {
-                var_dump($newGrid['row_' . $y][$x]);
-                var_dump($y);
-                $possibleWin = true;
+            $maxY = 20;
+            for ($tmp = 1; $tmp < 21; $tmp++) {
+                if ($newGrid['row_' . $tmp][$x] == '#') {
+                    $maxY = $tmp - 1;
+                    break;
+                }
+            }
+            $y = $maxY;
+            $x2 = $x + 1;
+            $maxY == 0 ? $possibleWin = false : $possibleWin = true;
+            while ($possibleWin && $y > ($maxY - 4) && $y > 0) {
                 for ($z = 0; $z < 10; $z++) {
-                    if (!$z == $x && $newGrid['row_' . $y][$z] != '#') {
+                    if ((!$z == $x) && ($newGrid['row_' . $y][$z] != '#')) {
                         $possibleWin = false;
                         break;
                     }
                 }
-                if ($possibleWin && $newGrid['row_' . ($y + 1)][$x] == '#'
-                    || $possibleWin && $newGrid['row_' . ($y + 2)][$x] == '#'
-                    || $possibleWin && $newGrid['row_' . ($y + 3)][$x] == '#') {
-                    $finalResults[$inputFileName] = 'BOOM ' . $x;
-                } else {
-                    $finalResults[$inputFileName] = 'NOPE';
-                }
-                $y++;
+                $y--;
             }
-            var_dump($finalResults[$inputFileName]);
-            var_dump($finalResults[$inputFileName] == $expected);
+            $possibleWin ? $finalResults[$inputFileName] = 'BOOM ' . $x2 : '';
+            array_key_exists($inputFileName, $finalResults) ? '' : $finalResults[$inputFileName] = 'NOPE';
         }
+        var_dump($finalResults[$inputFileName]);
+        var_dump($finalResults[$inputFileName] == $expected[0]);
+        echo '<hr>';
     }
 }
 
