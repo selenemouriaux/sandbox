20b98701f24216c35f10af37521f8a66c539a62f
Sélène Mouriaux <magicneuron@gmail.com>
2022-01-11 15:55:00 +0100

    dfhdfshb

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# sandbox
-sandbox
+# LIZ E. MYERS : Vitrine Auteur & plateforme sociale
+### Site de promotion des activités de l'auteure.
+Le but du site est de promouvoir **les livres** et **l'auteure** au travers d'outils déstinés à :
+1. Bibliothèque de livres :
+* moteur de recherche
+* illustrations / résumé
+* lien achat (ou intérgation Amazon, voir API)
+* pseudo visionneuse avec aperçu / extrait
+* partager vignette en plusieur formats
+2. Chroniques twitch & youtube :
+* live réguliers (embed ou lien selon possibilités API)
+* planning des live
+* potentielle participation à l'élaboration des sujets
+3. Liens services & collaborateurs :
+* traduction
+* illustrateur (contact et encart pro)
+* correcteur (contact et encart pro)
+4. Sociaux & About :
+* fb, twitter, insta...
+* à propos de l'auteure
+* contact
+5. Coin des lecteurs :
+* chat
+* rating &  commentaires
+6. Actu & Projets :
+* à paraître
+* aside de news?.. 
--- module1/php/tp-formulaires/persitent-contacts/persitent-contacts.php
+++ module1/php/tp-formulaires/persitent-contacts/persitent-contacts.php
@@ -1,15 +1,55 @@
 <?php
 
-function accessOrCreate($filePath) {
-    $emails = fopen($filePath, "a+");
-    return $emails;
+const FILEPATH = './newsletterSubs.csv';
+$newsletterSubs = getListFromCsv(FILEPATH) ?? null;
+$error = null;
+
+function getListFromCsv($filePath)
+{
+    $subs = [];
+    $emails = fopen(FILEPATH, "a+");
+    while (!feof($emails) && $data = fgetcsv($emails)) {
+        $subs[] = $data[0];
+    }
+    fclose($emails);
+    return $subs;
 }
-$emails = accessOrCreate('./newsletterSubs.csv');
-$newsletterSubs = fgetcsv($emails);
 
-$newSub = $_POST['email'] ?? null;
-$newSub ? $newsletterSubs[] = $newSub : null;
-fclose($emails);
+function addToCsvFile($email, $filePath)
+{
+    $emails = fopen($filePath, "a");
+    fputcsv($emails, [$email]);
+    fclose($emails);
+}
 
+function isNotRegisteredYet($email, $filePath)
+{
+    $arr = getListFromCsv($filePath);
+    if (in_array($email, $arr)) {
+        $GLOBALS['error'] = "Impossible, cet email est déjà utilisé.";
+    }
+    return in_array($email, $arr);
+}
+
+function processSubmission($sub)
+{
+    if (preg_match("#^[a-zéàè\-\.]{1,20}[@][a-zéèà\-]{1,20}[\.][a-zéàè]{2,3}([\.][a-zéèà]{0,3})?$#", $sub)) {
+        return trim($sub);
+    } elseif (empty($sub)) {
+        $GLOBALS['error'] = "Merci de renseigner un email";
+    } else {
+        $GLOBALS['error'] = "Format d'email invalide";
+    }
+    return null;
+}
+
+if ($_POST) {
+    $error = null;
+    if ($email = processSubmission($_POST['email'])) {
+        !isNotRegisteredYet($email, FILEPATH) ?
+            addToCsvFile($email, FILEPATH) : null;
+        $newsletterSubs = getListFromCsv(FILEPATH);
+    }
+}
 
 include 'persitent-contacts.phtml';
