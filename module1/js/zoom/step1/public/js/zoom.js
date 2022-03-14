f04e70401df6e2bf815b881c5fa253e45bd85d00
Sélène Mouriaux <magicneuron@gmail.com>
2022-03-14 15:25:21 +0100

    xtfgjhgf

--- module1/js/zoom/step1/public/js/zoom.js
+++ module1/js/zoom/step1/public/js/zoom.js
@@ -22,15 +22,17 @@ let zoomFrame;
  * Chargement de la version agrandie
  */
 function OnclickThumbnail(e) {
-    let split = zoom['src'].split('medias/')[1];
-    document.querySelector('.zoom-thumbnail[src*="'+split+'"]').classList.remove("is-active");
-    zoomFrame.classList.remove("animate__zoomIn");
-    zoomFrame.classList.add("animate__zoomOut");
+    // let split = zoom[e.dataset.frameNumber]['src'].split('medias/')[1];
+    // console.log(e['src'])
+    document.querySelector('.zoom-thumbnail.is-active').classList.remove("is-active");
+    zoomFrame[e.dataset.frameNumber].classList.remove("animate__zoomIn");
+    zoomFrame[e.dataset.frameNumber].classList.add("animate__zoomOut");
+    console.log(zoom)
     setTimeout(() => {
-        zoom['src'] = e['src'];
+        zoom[e.dataset.frameNumber]['src'] = e['src'];
         e.classList.add("is-active");
-        zoomFrame.classList.remove("animate__zoomOut");
-        zoomFrame.classList.add("animate__zoomIn");
+        zoomFrame[e.dataset.frameNumber].classList.remove("animate__zoomOut");
+        zoomFrame[e.dataset.frameNumber].classList.add("animate__zoomIn");
     }, 350);
 }
 
@@ -39,12 +41,20 @@ function OnclickThumbnail(e) {
 // ====================================================== //
 
 document.addEventListener('DOMContentLoaded', function () {
-    zoom = document.querySelector('.zoom-picture');
-    zoomFrame = document.querySelector('.zoom-figure');
+    zoom = document.querySelectorAll('.zoom-picture');
+    zoomFrame = document.querySelectorAll('.zoom-figure');
     images = document.querySelectorAll('.zoom-thumbnail');
     console.log(images);
     images.forEach(function (image) {
         image.addEventListener('click', function (e) {
+            if (image['src'].includes('galery1')) {
+                console.log("ca marche, frame 1")
+                image.dataset.frameNumber = 0;
+            } else {
+                console.log("ca marche, frame 2")
+                image.dataset.frameNumber = 1;
+            }
+            console.log(image)
             OnclickThumbnail(e.currentTarget);
         });
     })
