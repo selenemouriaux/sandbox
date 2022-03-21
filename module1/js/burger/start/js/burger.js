6c8d44c270bd66c2e9edc0290f603aaeeea5f9d8
Sélène Mouriaux <magicneuron@gmail.com>
2022-03-21 20:01:04 +0100

    burger

---
+++ module1/js/burger/start/js/burger.js
@@ -0,0 +1,24 @@
+;(function(){
+
+    function toggleNav() {
+
+        // Bonus : Recharger la page si la fenêtre est redimensionnée
+        
+
+        // 1 - Menu burger uniquement en dessous de 1024px (éviter un else)
+
+
+        // 2 - Création du bouton burger
+
+
+        // 3 - Insertion du bouton avant la nav, juste après .site-description
+        
+
+        // 4 - Gestionnaire d'évenement sur le bouton
+
+    }
+
+    // 6 - Chargement de la fonction toggleNav lorsque toutes les ressources sont chargées
+    
+
+})();
