f43cd0a9d85d3846e4b65f08004aa0b478424614
Sélène Mouriaux <magicneuron@gmail.com>
2022-03-22 15:57:19 +0100

    dsgvdf

--- module1/js/burger/start/js/burger.js
+++ module1/js/burger/start/js/burger.js
@@ -1,24 +1,47 @@
-;(function(){
+'use strict';
 
-    function toggleNav() {
+const btnBurger = document.querySelector('.button-burger');
+const openNav = document.querySelector('#menu');
 
-        // Bonus : Recharger la page si la fenêtre est redimensionnée
-        
-
-        // 1 - Menu burger uniquement en dessous de 1024px (éviter un else)
-
-
-        // 2 - Création du bouton burger
-
-
-        // 3 - Insertion du bouton avant la nav, juste après .site-description
-        
-
-        // 4 - Gestionnaire d'évenement sur le bouton
-
+;(function () {
+  // Bonus : Recharger la page si la fenêtre est redimensionnée
+  window.onresize = function () {
+    location.reload(true);
+    displayCollapsed(window);
+  }
+  // 1 - Menu burger uniquement en dessous de 1024px (éviter un else)
+  function displayCollapsed(box= window) {
+    openNav.style.display = 'none';
+    btnBurger.style.display = 'initial';
+    if (box.innerWidth > 1023) {
+      openNav.style.display = 'initial';
+      btnBurger.style.display = 'none';
+      return false;
     }
+    return true;
+  }
 
-    // 6 - Chargement de la fonction toggleNav lorsque toutes les ressources sont chargées
-    
+  displayCollapsed(this);
+
+  function toggleNav() {
+    openNav.classList.toggle('is-open');
+    displayCollapsed(this) ? openNav.style.display = 'initial' : openNav.style.display = 'none';
+  }
+
+  // 6 - Chargement de la fonction toggleNav lorsque toutes les ressources sont chargées
+
+  document.addEventListener('DOMContentLoaded', function () {
+
+    // installation des listeners d'animation de hovering
+    btnBurger.addEventListener('mouseenter', function () {
+      this.classList.add('has-focus');
+    })
+    btnBurger.addEventListener('mouseleave', function () {
+      this.classList.remove('has-focus');
+    })
+
+    // installation du listener d'appel de fonction
+    btnBurger.addEventListener('click', toggleNav);
+  })
 
 })();
